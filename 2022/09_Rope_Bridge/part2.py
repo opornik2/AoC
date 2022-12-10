439181beafaa35922ab97a05c16061d41d4df480
opornik2 <opornik@gmail.com>
2022-12-11 00:11:20 +0100

    corrections

--- 2022/09_Rope_Bridge/part2.py
+++ 2022/09_Rope_Bridge/part2.py
@@ -7,46 +7,31 @@ def dist(t,h):
     return max(abs(t[0]-h[0]), abs(t[1]-h[1]))
 
 
-def right(steps):
-    r[knot][0] += 1   # if too long
+def right(knot):
+    r[knot][0] += 1   # if > 2 then move right
+    if r[knot][1] != r[knot-1][1]:  # if y != Y then Y equal to n-1
+        r[knot][1] = r[knot-1][1]  #if diagonal
+
+
+def left(knot):
+    r[knot][0] -= 1   # if too long
     if r[knot][1] != r[knot-1][1]:
         r[knot][1] = r[knot-1][1]  #if diagonal
 
-    seen[r[9][0]][r[9][1]] = 1
-    for s in range(1, steps+1):
-        r[0][0] += 1
-        for knot in range(1,10):
-            if dist(r[knot-1], r[knot]) > 1:
-                r[knot][0] += 1   # if too long
-                if r[knot][1] != r[knot-1][1]: r[knot][1] = r[knot-1][1]  #if diagonal
-        seen[r[9][0]][r[9][1]] = 1
 
-def left(steps):
-    seen[t[0]][t[1]] = 1
-    for s in range(1, steps+1):
-        h[0] -= 1
-        if dist(h,t) > 1:
-            t[0] -= 1   # if too long
-            if t[1] != h[1]: t[1] = h[1]  #if diagonal
-        seen[t[0]][t[1]] = 1
+def up(knot):
+    r[knot][1] += 1   # if too long
+    if r[knot][0] != r[knot-1][0]:
+        r[knot][0] = r[knot-1][0]  #if diagonal
 
-def up(steps):
-    seen[t[0]][t[1]] = 1
-    for s in range(1, steps+1):
-        h[1] += 1
-        if dist(t,h) > 1:
-            t[1] += 1
-            if t[0] != h[0]: t[0] = h[0]
-        seen[t[0]][t[1]] = 1
 
-def down(steps):
-    seen[t[0]][t[1]] = 1
-    for s in range(1, steps+1):
-        h[1] -= 1
-        if dist(t,h) > 1:
-            t[1] -= 1   # if not covering
-            if t[0] != h[0]: t[0] = h[0]  #if diagonal
-        seen[t[0]][t[1]] = 1
+def down(knot):
+    r[knot][1] -= 1   # if too long
+    if r[knot][0] != r[knot-1][0]:
+        r[knot][0] = r[knot-1][0]  #if diagonal
+
+
+###############################################################################
 
 
 with open(sys.argv[1], mode='r') as input:
@@ -57,24 +42,27 @@ seen = defaultdict(dict)
 count = 0
 
 for line in fi:
+    print(f"\nline: {line}")
+    #print(f"before {r}")
     (dire, steps) = line.split(" ")
     steps = int(steps)
     seen[r[9][0]][r[9][1]] = 1
-    for s in range(1, steps+1):
-        r[0][0] += 1
+    for _ in range(1, steps+1):
+        if dire   == "R": r[0][0] += 1
+        elif dire == "L": r[0][0] -= 1
+        elif dire == "U": r[0][1] += 1
+        elif dire == "D": r[0][1] -= 1
         for knot in range(1,10):
+            di = dist(r[knot-1], r[knot])
+            print(f"dist  {knot-1}-{knot} = {di}")
             if dist(r[knot-1], r[knot]) > 1:
-                if dire == "R": right(steps)
-                elif dire == "D": down(steps)
-                elif dire == "L": left(steps)
-                elif dire == "U": up(steps)
+                if dire   == "R": right(knot)
+                elif dire == "L": left(knot)
+                elif dire == "U": up(knot)
+                elif dire == "D": down(knot)
+        print(f"middle {r}")
         seen[r[9][0]][r[9][1]] = 1
-
-    if dire == "R": right(steps)
-    elif dire == "D": down(steps)
-    elif dire == "L": left(steps)
-    elif dire == "U": up(steps)
-    print(f"H={h[0]}.{h[1]}  T={t[0]}.{t[1]}")
+    #print(f"after  {r}")
 
 for k in seen.keys():
     for n in seen[k].keys():
