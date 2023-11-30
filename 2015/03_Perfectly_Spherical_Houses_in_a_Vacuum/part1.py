74f54634afcd876fed9fc9375aa18651ad63b910
opornik2 <opornik@gmail.com>
2023-11-30 09:15:07 +0100

    2015 day 3-part1 done

---
+++ 2015/03_Perfectly_Spherical_Houses_in_a_Vacuum/part1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict
+
+with open(sys.argv[1], mode='r') as infile:
+     t = infile.read().strip()
+
+x=1000
+y=1000
+cursor=[x,y]
+visited = defaultdict(dict)
+visited[str(cursor[:])] = True
+
+for char in t:
+    if "^" in char:
+        y+=1
+    elif "v" in char:
+        y-=1
+    elif ">" in char:
+        x+=1
+    elif "<" in char:
+        x-=1
+    
+    cursor=[x,y]
+    visited[str(cursor[:])] = True
+print(len(visited))
+
