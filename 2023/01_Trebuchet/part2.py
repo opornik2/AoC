cd29d45ecc7adc6b9d85bea15e4d93550e1b33c7
opornik2 <opornik@gmail.com>
2023-12-01 12:29:55 +0100

    2023 cosmetic changes

--- 2023/01_Trebuchet/part2.py
+++ 2023/01_Trebuchet/part2.py
@@ -17,27 +17,17 @@ for l in t:
         if re.search(pat, l):
             match = re.search(pat, l)
             digit = int(match.group(0))
-        elif re.search('^one', l):
-            digit = 1
-        elif re.search('^two', l):
-            digit = 2
-        elif re.search('^three', l):
-            digit = 3
-        elif re.search('^four', l):
-            digit = 4
-        elif re.search('^five', l):
-            digit = 5
-        elif re.search('^six', l):
-            digit = 6
-        elif re.search('^seven', l):
-            digit = 7
-        elif re.search('^eight', l):
-            digit = 8
-        elif re.search('^nine', l):
-            digit = 9
+        elif re.search('^one', l):   digit = 1
+        elif re.search('^two', l):   digit = 2
+        elif re.search('^three', l): digit = 3
+        elif re.search('^four', l):  digit = 4
+        elif re.search('^five', l):  digit = 5
+        elif re.search('^six', l):   digit = 6
+        elif re.search('^seven', l): digit = 7
+        elif re.search('^eight', l): digit = 8
+        elif re.search('^nine', l):  digit = 9
+        if digit > 0: digits.append(digit)
         l = l[1:]
-        if digit > 0:
-            digits.append(digit)
     digit1 = digits[0]
     digit2 = digits[-1]
     summ += 10*int(digit1) + int(digit2)
