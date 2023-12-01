fc66fae105384dd3fcfb8993712782b3e41f682c
opornik2 <opornik@gmail.com>
2023-12-01 12:25:37 +0100

    2023 removed unnecessary list

--- 2023/01_Trebuchet/part2.py
+++ 2023/01_Trebuchet/part2.py
@@ -6,10 +6,7 @@ import re
 with open(sys.argv[1], mode='r') as input:
     t = input.read().strip().split("\n")
 
-
-
 summ = 0
-words = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
 for l in t:
     print(l)
     digits=[]
